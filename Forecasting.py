32753ed65219dddddf40198d8263fe7e84607188
gennis9 <95836358+gennis9@users.noreply.github.com>
2022-10-08 13:30:38 +0800

    Time Series Forecasting Models

--- Forecasting.py
+++ Forecasting.py
@@ -20,117 +20,118 @@ import matplotlib.pyplot as plt
 # Estimate the index return rate of S&P 500 by ARMA.
 
 ## Get the index data from Yahoo Finance.
-spy = ffn.get("^GSPC", start = '2015-01-01', end = '2021-12-31')
+Data = ffn.get("^GSPC", start = '2015-01-01', end = '2022-09-30')
 ## Sketch the index rate.
-spy.plot.line(grid = True)
+Data.plot.line(grid = True)
 
 ## Calculate the return rates of S&P 500 index.
-spy["Return rate"] = spy['gspc'].pct_change()
-spy.dropna(inplace = True)
+Data["Return Rate"] = Data['gspc'].pct_change()
+Data.dropna(inplace = True)
 
 ## Run Augmented Dickey-Fuller unit root test.
 # The test can be used to test for a unit root in a univariate process in the presence of serial correlation.
 # The 1st element in the result is the test statistic (adf); the 2nd is p-value; the 3rd is number of lags used; the 4th is number of observations; the 5th dict is critical values for the test statistic at the 1 %, 5 %, and 10 % levels; the last one is the maximized information criterion if autolag is not None.
-sm.tsa.stattools.adfuller(spy["Return rate"].dropna())
+sm.tsa.stattools.adfuller(Data["Return Rate"].dropna())
 
 ## Draw the histogram to show the return rates.
-spy["Return rate"].plot.hist(density = True, bins = 100)
+Data["Return Rate"].plot.hist(density = True, bins = 100)
 
 ## Run Kolmogorov-Smirnov test for goodness of fit.
-loc, scale = norm.fit(spy["Return rate"])
+loc, scale = norm.fit(Data["Return Rate"])
 n = norm(loc = loc, scale = scale)
-kstest(spy["Return rate"].dropna(), n.cdf)
+print( kstest(Data["Return Rate"].dropna(), n.cdf) )
 
 ## Plot the autocorrelation function. Plots lags on the horizontal and the correlations on vertical axis. The shaped area is the confidence interval (95% default) and the bar exceeded the shaped area represent autocorrelation significantly.
-smg.tsaplots.plot_acf(spy["Return rate"], lags = 20)
+smg.tsaplots.plot_acf(Data["Return Rate"], lags = 20)
 ## Plot the partial autocorrelation function.
-smg.tsaplots.plot_pacf(spy["Return rate"], lags = 20)
+smg.tsaplots.plot_pacf(Data["Return Rate"], lags = 20)
 
 ## Compute information criteria for many ARMA models.
-tsa.stattools.arma_order_select_ic(spy["Return rate"], ic = ['aic', 'bic'], trend = 'nc')
+tsa.stattools.arma_order_select_ic(Data["Return Rate"], ic = ['aic', 'bic'], trend = 'nc')
 
 ## Run ARMA(2, 2) model on the return rates and fit it using exact maximum likelihood via Kalman filter.
-model = sm.tsa.ARMA(spy["Return rate"].dropna(), (2, 2)).fit(disp = False)
-print(model.summary())
+_result = sm.tsa.ARMA(Data["Return Rate"].dropna(), (2, 2)).fit(disp = False)
+print(_result.summary())
 
 ## Plot forecasts of the time-series data under the regressed result of ARMA model.
-model.plot_predict(start = 1600, end = 1780)
+plt.figure(figsize=(6,10))
+_result.plot_predict(start = 1800, end = 1970)
 
 
 
 ### Generalized Autoregressive Conditional Heteroskedasticity (GARCH) Model
 ## Get the price data of S&P 500 from Yahoo Finance.
-spy = yf.download("^gspc", start = "2020-01-01", end = "2021-12-31")
+Data = yf.download("^gspc", start = "2022-01-01", end = "2022-09-30")
 ## Calculate the return rates of the index.
-return_rates = spy["Close"].pct_change().dropna()
+ReturnRates = Data["Close"].pct_change().dropna()
 
 ## Show the return rates and the squared return rates that are employed in GARCH model.
-return_rates.plot(grid = True)
-(return_rates ** 2).plot(grid = True)
+ReturnRates.plot(grid = True)
+(ReturnRates ** 2).plot(grid = True)
 
 ## Run the GARCH(1, 1) model on the return rates and show the result table.
-model = arch_model(return_rates * 100, vol = 'garch', p = 1, o = 0, q = 1, dist = 'Normal')
-result = model.fit()
-print(result.summary())
+_model = arch_model(ReturnRates * 100, vol = 'garch', p = 1, o = 0, q = 1, dist = 'Normal')
+_result = _model.fit()
+print(_result.summary())
 
 ## Plot the standardized residuals and the conditional volatility of the GARCH model run.
-fig = result.plot(annualize = 'D')
+fig = _result.plot(annualize = 'D')
 fig.set_size_inches(12, 6)
 
 ## Run the GARCH(2, 1) model on the return rates and show the result table.
-model2 = arch_model(return_rates * 100, vol = 'garch', p = 2, o = 0, q = 1, dist = 'Normal')
-result2 = model2.fit()
-print(result2.summary())
+_model2 = arch_model(ReturnRates * 100, vol = 'garch', p = 2, o = 0, q = 1, dist = 'Normal')
+_result2 = _model2.fit()
+print(_result2.summary())
 
 ## Plot the standardized residuals and the conditional volatility of GARCH(2, 1).
-fig = result2.plot(annualize = 'D')
-fig.set_size_inches(12, 6)
+fig = _result2.plot(annualize = 'D')
+fig.set_size_inches(8, 6)
 
 ## Show the forecasted variance.
-yhat = result2.forecast(horizon = 10)
-plt.plot(yhat.variance.values[-1, :])
+_yhat = _result2.forecast(horizon = 10)
+plt.plot(_yhat.variance.values[-1, :])
 
 
 
 ### Vector Autoregression (VAR)
 ## Download the macroeconomic data of the US from pandas database.
-mdata = sm.datasets.macrodata.load_pandas().data
+Data = sm.datasets.macrodata.load_pandas().data
 ## Extract the time data.
-dates = mdata[['year', 'quarter']].astype(int).astype(str)
+Dates = Data[['year', 'quarter']].astype(int).astype(str)
 ## Generate quarter data.
-quarterly = dates["year"] + "Q" + dates["quarter"]
+Quarterly = Dates["year"] + "Q" + Dates["quarter"]
 
 ## Convert the quarter data to datatime format.
-quarterly = tsa.base.datetools.dates_from_str(quarterly)
+Quarterly = tsa.base.datetools.dates_from_str(Quarterly)
 ## Extract the data of real GDP, real comsumption, and real investment.
-mdata = mdata[['realgdp','realcons','realinv']]
+Data = Data[['realgdp','realcons','realinv']]
 ## Employ the timestamp data as the index of the macroeconomic data to be investigated.
-mdata.index = pd.DatetimeIndex(quarterly)
+Data.index = pd.DatetimeIndex(Quarterly)
 
 ## Calculate the percentage rate of change of the macroeconomic data.
-data = np.log(mdata).diff().dropna()
+Percent = np.log(Data).diff().dropna()
 
 ## Fit VAR(1) process and do lag order selection.
-model = tsa.api.VAR(data)
-results = model.fit(1)
-results.summary()
+_model = tsa.api.VAR(Percent)
+_results = _model.fit(1)
+_results.summary()
 
 ## Plot the results of the macroeconomic data under the VAR(1) process.
-results.plot()
+_results.plot()
 
 ## Choose the order(p) of the VAR model based on each of the available information criteria with the lowest scores attained and with maximum 15 lag orders.
 # The available information criteria include AIC, BIC, FPE, and HQIC in this case.
-result_all = model.select_order(15)
+_result_all = _model.select_order(15)
 ## Demonstrate the result.
-print(result_all.summary())
+print(_result_all.summary())
 
 ## Choose the order(p) of the VAR model using the lowest Bayesian information criterion with maximum 15 lag orders.
-results = model.fit(maxlags = 15, ic = "bic")
+_results = _model.fit(maxlags = 15, ic = "bic")
 
 ## Get the order of the VAR process.
-lag_order = results.k_ar
+lagOrder = _results.k_ar
 ## Produce linear minimum MSE forecasts for desired number of steps ahead, using the values with the order chosen by the lowest BIC score.
-results.forecast(data.values[-lag_order:], 5)
+_results.forecast(Percent.values[-lagOrder:], 5)
 
 ## Plot the forecasting of the macrodata, real GDP, real comsumption, and real investment, with 10 periods.
-results.plot_forecast(10)
+_results.plot_forecast(10)
